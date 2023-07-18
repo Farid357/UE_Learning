9d1909b0e032a8278d7d0a12516671c736d73249
Farid Yusubov <Blackwazz7@gmail.com>
2023-07-19 03:55:49 +0500

    Plugins Added

---
+++ Source/UE_Learning/UE_Learning.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UE_Learning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_Learning, "UE_Learning" );
